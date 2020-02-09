bf4ac72ec22ab457d32ac35946e4cfe2d2ae824d
Williano <paawilly17@gmail.com>
2020-02-09 02:36:57 -0600

    [NEW FEATURE] :sparkles: Adding colors and custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("Hey Ninjas!"),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("My First App"),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+          "Hello Ninjas",
+        style: TextStyle(fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[500],
+          fontFamily: "IndieFlower"
+         ),
+
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text("click"),
+      onPressed: (){},
+      backgroundColor: Colors.red[600],
+    ),
+  ),
 ));
 
