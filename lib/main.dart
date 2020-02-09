4b25e0321b9c4f494dac6eaa720b7d674d4b44eb
Williano <paawilly17@gmail.com>
2020-02-09 03:51:12 -0600

    [NEW FEATURE] :sparkles: Add code for buttons and Icons

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,32 @@ class Home extends StatelessWidget {
 //           image: AssetImage("assets/images/snow.jpg"),
 ////          image: NetworkImage("https://images.unsplash.com/photo-1581115684447-383df67ff1ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI2Njg5fQ&auto=format&fit=crop&w=934&q=80") ,
 //        )
-      child: Image.asset("assets/snow.jpg"),
+//      child: Image.asset("assets/snow.jpg"),
+//
+//      child: RaisedButton(
+//        onPressed: () {},
+//        child: Text(
+//          "Click Me"
+//        ),
+//        color: Colors.blue,
+//      ),
+//      child: FlatButton(
+//        onPressed: () {},
+//        child: Text(
+//          "Click"
+//        ),
+//        color: Colors.lightBlue,
+//      ),
+//      child: RaisedButton.icon(onPressed: () {}, icon: Icon(
+//        Icons.mail
+//      ), label: Text("Mail me"),
+//        color: Colors.amber,),
+
+      child: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.alternate_email),
+        color: Colors.amber,
+      ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Text("click"),
