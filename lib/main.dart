208a6c161ee6477113f2edf6aa8a1cba87e03a92
Williano <paawilly17@gmail.com>
2020-02-09 02:49:41 -0600

    [NEW FEATURE] :sparkles: Add Stateless Widget and Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("My First App"),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-          "Hello Ninjas",
-        style: TextStyle(fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[500],
-          fontFamily: "IndieFlower"
-         ),
-
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text("click"),
-      onPressed: (){},
-      backgroundColor: Colors.red[600],
-    ),
-  ),
+  home: Home()
 ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          "Hello Ninjas",
+          style: TextStyle(fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[500],
+              fontFamily: "IndieFlower"
+          ),
+
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("click"),
+        onPressed: (){},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
+ 
