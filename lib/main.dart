23494cc53ccbea0dc1af0d7b53789b10b437d940
Williano <paawilly17@gmail.com>
2020-02-12 21:58:49 -0600

    Add code for how to use rows

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,26 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body:
-          Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Text("Hello"),
-          ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text("Hello World"),
+          FlatButton(
+            onPressed: () {},
+            color: Colors.amber,
+            child: Text('Click Me')),
+            Container(
+              color: Colors.cyan,
+              padding: EdgeInsets.all(30.0),
+              child: Text("Inside Container"),
+          )
+        ],
+      ),
+//          Padding(
+//            padding: EdgeInsets.all(30.0),
+//            child: Text("Hello"),
+//          ),
 //      Container(
 //        color: Colors.grey[400],
 //        child: Text("Hello"),
